d6dca776fecb5507c1d8a668be88fc2185666ed8
anaramanli <amanlianar@gmail.com>
2024-05-07 02:20:08 +0400

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Shop()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AboutUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+app.Run();
